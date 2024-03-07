6c1f2978cdb8c7959ce34c4cc47ef542f8f2ced5
RichardRain <alexander.hell.scorpion@gmail.com>
2024-03-07 19:05:39 +0300

    Реализована структура сайта, полностью реализована шапка и подвал сайта, реализована секция 'team-section' для мобильных устройств. ToDo: Подправить блок 'hero-section', доработать остальные секции.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,5 @@
-import '../pages/index.css'; // eslint-disable-line
-
+import '../pages/index.css';
+import '../images/ui.svg';
+import '../images/logo.svg';
+import '../images/icons.svg';
+import '../images/socials.svg';
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
 
 module.exports = {
@@ -9,9 +10,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-        publicPath: ''
+    publicPath: ''
   },
-    mode: 'development',
+  mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'),
     compress: true,
@@ -26,17 +27,21 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|jpg|jpeg|gif|webp|avif)$/i,
         type: 'asset/resource',
         generator: {
-            filename: 'images/[name].[hash][ext]',
+          filename: 'images/[name].[hash][ext]',
         }
+      },
+      {
+        test: /\.svg$/,
+        use: "svg-sprite-loader",
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf)$/i,
         type: 'asset/resource',
         generator: {
-            filename: 'fonts/[name].[hash][ext]',
+          filename: 'fonts/[name].[hash][ext]',
         }
       },
       {
@@ -45,7 +50,7 @@ module.exports = {
           loader: 'css-loader',
           options: { importLoaders: 1 }
         },
-      'postcss-loader']
+          'postcss-loader']
       },
     ]
   },
@@ -54,6 +59,7 @@ module.exports = {
       template: 'src/index.html'
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new SpriteLoaderPlugin()
   ]
 };
